3ee094c1f81e0b49b8ce6cf648f4e0fa19460729
Aom Siriwat <siriwat576@gmail.com>
2023-11-14 15:14:44 +0700

    feat: Add TaskController and usecases with TaskService
    This commit adds the TaskController and several usecases, including AcceptUsecase, CompleteUsecase, CreateUsecase, CronUsecase, InfoUsecase, and ListUsecase, to the TaskModule. It also includes the PrismaService and TaskService as providers.

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,10 +1,30 @@
 import { Module } from "@nestjs/common";
+import { TaskController } from "./task.controller";
+import { AcceptUsecase } from "./usecases/accept.usecase";
+import { CompleteUsecase } from "./usecases/complete.usecase";
+import { CreateUsecase } from "./usecases/create.usecase";
+import { CronUsecase } from "./usecases/cron.usecase";
+import { InfoUsecase } from "./usecases/info.usecase";
+import { ListUsecase } from "./usecases/list.usecase";
+import { TaskService } from "./task.service";
+import { PrismaService } from "@/common/services/prisma.service";
 
 @Module({
     imports: ([]).concat(
-        
     ),
-    controllers: [],
-    providers: [],
+    controllers: [
+        TaskController
+    ],
+    providers: [
+        AcceptUsecase,
+        CompleteUsecase,
+        CreateUsecase,
+        CronUsecase,
+        InfoUsecase,
+        ListUsecase,
+
+        PrismaService,
+        TaskService,
+    ],
 })
 export class TaskModule {}
